efafbda7f29a4801e8d1adbc98702fe71131e9c1
zyg <932186976@qq.com>
2019-05-30 14:38:25 +0800

    20190529培训

--- src/helloword/helloworld.py
+++ src/helloword/helloworld.py
@@ -1,2 +1,33 @@
-print("helloword")
 
+def demo(num,num_list):
+    print("函数内部")
+    num = 100
+    num_list = [1,2,3]
+    print(num)
+    print(num_list)
+    print("代码完成")
+
+glnum = 200
+glnum_list = [4,5,6]
+#demo(glnum,glnum_list)
+#print(glnum)
+#print(glnum_list)
+
+def demo2(num_list):
+    num_list.extend([1,2,3])
+    print(num_list)
+
+#demo2(glnum_list)
+#print(glnum_list)
+
+def demo3(num,num_list):
+    print("开始")
+    num += num
+    num_list += num_list#列表+=的本质是在调用extend方法
+    print(num)
+    print(num_list)
+    print("完成")
+
+demo3(glnum,glnum_list)
+print(glnum)
+print(glnum_list)
--- src/helloword/函数基础.py
+++ src/helloword/函数基础.py
@@ -1,6 +1,6 @@
 # -*- coding:utf-8 -*-
 __author__ = '朱永刚'
-
+'''
 num = 0
 
 def sun_numbers(*args):
@@ -16,3 +16,59 @@ def person(bixu,*args,moren = '默认',mmgjz,**kwargs):
     print(bixu,moren,args,mmgjz,kwargs)
 
 person('必须',1,2,3,moren='moren',mmgjz='命名关键字',a=1,b=2,c=3)
+'''
+
+
+
+def sum_numbers(*args):
+    sum = 0
+    for temp in args:
+        sum += temp
+    return sum,temp
+
+print(type(sum_numbers(1,2,3)))
+print(sum_numbers(1,3,4,5))
+
+
+
+'''
+def demo(num, num_list):
+	print("函数内部")
+	# 赋值语句
+	num = 200
+	num_list = [1, 2, 3]
+	print(num)
+	print(num_list)
+	print("函数代码完成")
+
+glnum = 99
+gllist = [4, 5, 6]
+demo(glnum, gllist)
+
+print(glnum)#200
+print(gllist)#[1,2,3]
+'''
+'''
+def demo(num, num_list):
+	print("函数内部代码")
+	num += num
+	# num_list.extend(num_list) 由于是调用方法，所以不会修改变量的引用
+	# 函数执行结束后，外部数据同样会发生变化
+	num_list += num_list
+	print(num)
+	print(num_list)
+	print("函数代码完成")
+glnum = 9
+gllist = [1, 2, 3]
+demo(glnum, gllist)
+print(glnum)
+print(gllist)'''
+
+def jiaohuan(a,b):
+    a,b = b,a
+    print(a,b)
+
+
+a = 1
+b = 2
+jiaohuan(a,b)
